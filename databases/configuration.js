cff34896ca1bd13cefdc5554ab8d6f663edb5968
Leo Mejia <leonardo.mejia@est.iudigital.edu.co>
2022-10-11 08:38:09 -0500

    :tada: first commit

--- databases/configuration.js
+++ databases/configuration.js
@@ -1,16 +1,17 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const mongoConn = async () => {
+    console.log(process.env.MONGODB_URI)
     try{
-       await mongoose.connect(process.env.MONGO_URI, {
+        await mongoose.connect( process.env.MONGODB_URI, {
             useNewUrlParser: true, 
             useUnifiedTopology: true
         });
-        console.log('conexión OK con Mongo!')
+        console.log('Conexión Mongo OK!');  
     }catch(e){
-        console.log('Error de conexión', e);
+        console.log('Error de conexión a Mongo', e);
         throw new Error('Error de conexión');
-    }
-}
+    }    
+};
 
-module.exports = { mongoConn }
+module.exports = { mongoConn };
